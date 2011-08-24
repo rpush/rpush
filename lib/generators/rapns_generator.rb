5e8fcf9a902cc82fe1293f02911bda474f9de2ec
Ian Leitch <port001@gmail.com>
2011-08-24 09:14:16 +0100

    Migration generator

--- lib/generators/rapns_generator.rb
+++ lib/generators/rapns_generator.rb
@@ -1,3 +1,12 @@
 class RapnsGenerator < Rails::Generators::Base
-  
+  include Rails::Generators::Migration
+  source_root File.expand_path('../templates', __FILE__)
+
+  def self.next_migration_number(path)
+    Time.now.utc.strftime("%Y%m%d%H%M%S")
+  end
+
+  def copy_migration
+     migration_template "create_rapns_notifications.rb", "db/migrate/create_rapns_notifications.rb"
+  end
 end
