18c6c7e767128c3e413b1ed14782c504e179d7e4
Ian Leitch <port001@gmail.com>
2011-10-08 22:22:00 +0100

    AR adapter can now be chosen with the ADAPTER environment variable.

--- Gemfile
+++ Gemfile
@@ -1,11 +1,13 @@
-source "http://rubygems.org"
+source 'http://rubygems.org'
 
-gem "rake"
-gem "rspec"
-gem "shoulda"
-gem "activerecord", :require => "active_record"
-gem "pg"
-gem "database_cleaner"
-gem "simplecov"
+gem 'rake'
+gem 'rspec'
+gem 'shoulda'
+gem 'activerecord', :require => 'active_record'
+gem 'pg'
+gem 'mysql2'
+gem 'mysql'
+gem 'database_cleaner'
+gem 'simplecov'
 
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,34 @@
-ENV["RAILS_ENV"] = "test"
+ENV['RAILS_ENV'] = 'test'
 
 require 'simplecov'
 SimpleCov.start do
   add_filter '/spec/'
 end
 
-require "active_record"
+require 'active_record'
+adapters = ['mysql', 'mysql2', 'postgresql']
+adapter = ENV['ADAPTER'] || 'postgresql'
 
-ActiveRecord::Base.establish_connection("adapter" => "postgresql", "database" => "rapns_test")
-require "generators/templates/create_rapns_notifications"
+if !adapters.include?(adapter)
+  puts "No such adapter '#{adapter}'. Valid adapters are #{adapters.join(', ')}."
+  exit 1
+end
+
+puts "Using #{adapter} adapter."
+ActiveRecord::Base.establish_connection('adapter' => adapter, 'database' => 'rapns_test')
+require 'generators/templates/create_rapns_notifications'
 
 CreateRapnsNotifications.down rescue ActiveRecord::StatementInvalid
 CreateRapnsNotifications.up
-
+module Rails; end
 Bundler.require(:default)
 
-require "shoulda"
+require 'shoulda'
 require 'database_cleaner'
 DatabaseCleaner.strategy = :truncation
 
-require "rapns"
-require "rapns/daemon"
+require 'rapns'
+require 'rapns/daemon'
 
 RSpec.configure do |config|
   config.before(:each) { DatabaseCleaner.clean }
