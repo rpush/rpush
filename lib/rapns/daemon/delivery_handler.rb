510b762811bc6ce20e2bcf3614c7b3a84a09b688
Ian Leitch <port001@gmail.com>
2011-11-02 10:21:01 +0000

    Split DeliveryHandler into smaller methods

--- lib/rapns/daemon/delivery_handler.rb
+++ lib/rapns/daemon/delivery_handler.rb
@@ -4,7 +4,7 @@ module Rapns
       STOP = 0x666
 
       def start
-        @thread = Thread.new do 
+        Thread.new do 
           loop do
             break if @stop
             handle_next_notification
@@ -18,32 +18,39 @@ module Rapns
 
       protected
 
+      def deliver(notification)
+        Rapns::Daemon.connection_pool.claim_connection do |connection|
+          begin
+            connection.write(notification.to_binary)
+
+            notification.delivered = true
+            notification.delivered_at = Time.now
+            notification.save!(:validate => false)
+
+            Rapns::Daemon.logger.info("Notification #{notification.id} delivered to #{notification.device_token}")
+          rescue Rapns::DeliveryError => error
+            handle_delivery_error(notification, error)
+          end
+        end
+      end
+
+      def handle_delivery_error(notification, error)
+        Rapns::Daemon.logger.error(error)
+
+        notification.delivered = false
+        notification.delivered_at = nil
+        notification.failed = true
+        notification.failed_at = Time.now
+        notification.error_code = error.code
+        notification.error_description = error.description
+        notification.save!(:validate => false)
+      end
+
       def handle_next_notification
         notification = Rapns::Daemon.delivery_queue.pop
         begin
           return if notification == STOP
-
-          Rapns::Daemon.connection_pool.claim_connection do |connection|
-            begin
-              connection.write(notification.to_binary)
-
-              notification.delivered = true
-              notification.delivered_at = Time.now
-              notification.save!(:validate => false)
-
-              Rapns::Daemon.logger.info("Notification #{notification.id} delivered to #{notification.device_token}")
-            rescue Rapns::DeliveryError => error
-              Rapns::Daemon.logger.error(error)
-
-              notification.delivered = false
-              notification.delivered_at = nil
-              notification.failed = true
-              notification.failed_at = Time.now
-              notification.error_code = error.code
-              notification.error_description = error.description
-              notification.save!(:validate => false)
-            end
-          end
+          deliver(notification)
         rescue StandardError => e
           Rapns::Daemon.logger.error(e)
         ensure
