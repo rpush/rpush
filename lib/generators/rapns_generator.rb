5a1b78792f637ed60e120307c60ebc24e8ed8400
Ian Leitch <port001@gmail.com>
2012-02-15 12:31:17 +0000

    Ensure generated migrations have unique timestamps

--- lib/generators/rapns_generator.rb
+++ lib/generators/rapns_generator.rb
@@ -3,7 +3,10 @@ class RapnsGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def self.next_migration_number(path)
-    Time.now.utc.strftime("%Y%m%d%H%M%S")
+    @time ||= Time.now.utc
+    @calls ||= -1
+    @calls += 1
+    (@time + @calls.seconds).strftime("%Y%m%d%H%M%S")
   end
 
   def copy_migration
