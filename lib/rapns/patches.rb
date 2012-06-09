0d60a0a8e1371b847d41be76297e8e20d9fa46de
Ben Taylor <taybenlor@gmail.com>
2012-06-09 16:28:54 +1000

    Switch the order of checks for patching to support ActiveRecord 3.2.5 ref to [this issue](https://github.com/ileitch/rapns/issues/43) in rapns.

--- lib/rapns/patches.rb
+++ lib/rapns/patches.rb
@@ -1,5 +1,5 @@
-if ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'postgresql'
-  if Rails::VERSION::STRING == '3.1.0' || Rails::VERSION::STRING == '3.1.1'
+if Rails::VERSION::STRING == '3.1.0' || Rails::VERSION::STRING == '3.1.1'
+  if ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'postgresql'
     STDERR.puts "[WARNING] Detected Rails #{Rails::VERSION::STRING}, patching PostgreSQLAdapter to fix reconnection bug: https://github.com/rails/rails/issues/3160"
     require "rapns/patches/rails/#{Rails::VERSION::STRING}/postgresql_adapter.rb"
   end
