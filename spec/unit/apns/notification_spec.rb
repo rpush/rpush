a44d7ff1c97467e227ee4953c9085b7bb62bcbf9
Ian Leitch <port001@gmail.com>
2012-10-18 21:55:04 -0700

    Support content-available. Fixes #68.

--- spec/unit/apns/notification_spec.rb
+++ spec/unit/apns/notification_spec.rb
@@ -106,6 +106,24 @@ describe Rapns::Apns::Notification, 'MDM' do
   end
 end
 
+describe Rapns::Apns::Notification, 'content-available' do
+  let(:notification) { Rapns::Apns::Notification.new }
+
+  it 'includes content-available in the payload' do
+    notification.content_available = true
+    notification.as_json['aps']['content-available'].should == 1
+  end
+
+  it 'does not include content-available in the payload if not set' do
+    notification.as_json['aps'].key?('content-available').should be_false
+  end
+
+  it 'does not include convert-available as a non-aps attribute' do
+    notification.content_available = true
+    notification.as_json.key?('content-available').should be_false
+  end
+end
+
 describe Rapns::Apns::Notification, "to_binary" do
   it "should correctly convert the notification to binary" do
     notification = Rapns::Apns::Notification.new
