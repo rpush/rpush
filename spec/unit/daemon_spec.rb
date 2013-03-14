4fa0f70217fda2af249a110fbedbdb09c7ae11e9
Maximilian Szengel <szengel@equinux.com>
2013-03-14 14:43:18 +0100

    Add support for switching the daemon logger.

--- spec/unit/daemon_spec.rb
+++ spec/unit/daemon_spec.rb
@@ -7,7 +7,8 @@ describe Rapns::Daemon, "when starting" do
   let(:certificate) { stub }
   let(:password) { stub }
   let(:config) { stub(:pid_file => nil, :airbrake_notify => false,
-    :foreground => true, :embedded => false, :push => false, :store => :active_record) }
+    :foreground => true, :embedded => false, :push => false,
+    :store => :active_record, :logger => nil) }
   let(:logger) { stub(:logger, :info => nil, :error => nil, :warn => nil) }
 
   before do
@@ -102,6 +103,7 @@ end
 
 describe Rapns::Daemon, "when being shutdown" do
   let(:config) { stub(:pid_file => '/rails_root/rapns.pid') }
+  let(:logger) { stub(:info => nil, :error => nil, :warn => nil) }
 
   before do
     Rapns.stub(:config => config)
