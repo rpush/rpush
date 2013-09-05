6d73195ee3598bfb3e5bdc92ee6fa3175911d167
Ian Leitch <port001@gmail.com>
2013-09-05 12:09:48 -0700

    Only perform cert private key validation for apns. Fixes #133.

--- lib/rapns/apns/app.rb
+++ lib/rapns/apns/app.rb
@@ -3,6 +3,22 @@ module Rapns
     class App < Rapns::App
       validates :environment, :presence => true, :inclusion => { :in => %w(development production sandbox) }
       validates :certificate, :presence => true
+      validate :certificate_has_matching_private_key
+
+      private
+
+      def certificate_has_matching_private_key
+        result = false
+        if certificate.present?
+          x509 = OpenSSL::X509::Certificate.new(certificate) rescue nil
+          pkey = OpenSSL::PKey::RSA.new(certificate, password) rescue nil
+          result = !x509.nil? && !pkey.nil?
+          unless result
+            errors.add :certificate, 'Certificate value must contain a certificate and a private key.'
+          end
+        end
+        result
+      end
     end
   end
 end
--- lib/rapns/app.rb
+++ lib/rapns/app.rb
@@ -10,22 +10,5 @@ module Rapns
 
     validates :name, :presence => true, :uniqueness => { :scope => [:type, :environment] }
     validates_numericality_of :connections, :greater_than => 0, :only_integer => true
-
-    validate :certificate_has_matching_private_key
-
-    private
-
-    def certificate_has_matching_private_key
-      result = false
-      if certificate.present?
-        x509 = OpenSSL::X509::Certificate.new(certificate) rescue nil
-        pkey = OpenSSL::PKey::RSA.new(certificate, password) rescue nil
-        result = !x509.nil? && !pkey.nil?
-        unless result
-          errors.add :certificate, 'Certificate value must contain a certificate and a private key.'
-        end
-      end
-      result
-    end
   end
 end
