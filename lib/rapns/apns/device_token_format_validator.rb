a745e217c426927070079a22918179bf729ed3bd
Ian Leitch <port001@gmail.com>
2013-11-11 16:34:22 +0200

    Ignore device token case. Fixes #166.

--- lib/rapns/apns/device_token_format_validator.rb
+++ lib/rapns/apns/device_token_format_validator.rb
@@ -3,7 +3,7 @@ module Rapns
     class DeviceTokenFormatValidator < ActiveModel::Validator
 
       def validate(record)
-        if record.device_token !~ /^[a-z0-9]{64}$/
+        if record.device_token !~ /^[a-z0-9]{64}$/i
           record.errors[:device_token] << "is invalid"
         end
       end
