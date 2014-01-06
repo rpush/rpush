210d9943a533b6f84c8f2b06628046e294df4d73
Matt Connolly <matt.connolly@me.com>
2014-01-06 21:01:36 +1000

    Update Rapns::App validation to not swallow unexpected exceptions during validation

--- spec/unit/app_spec.rb
+++ spec/unit/app_spec.rb
@@ -13,4 +13,18 @@ describe Rapns::App do
     app = Rapns::Gcm::App.new(:name => 'test', :environment => 'production', :auth_key => TEST_CERT)
     app.valid?.should be_true
   end
+
+  context 'validating certificates' do
+    it 'rescues from certificate error' do
+      app = Rapns::Apns::App.new(:name => 'test', :environment => 'development', :certificate => 'bad')
+      expect{app.valid?}.not_to raise_error
+      expect(app.valid?).to be_false
+    end
+
+    it 'raises other errors' do
+      app = Rapns::Apns::App.new(:name => 'test', :environment => 'development', :certificate => 'bad')
+      OpenSSL::X509::Certificate.stub(:new).and_raise(NameError, 'simulating no openssl')
+      expect{app.valid?}.to raise_error(NameError)
+    end
+  end
 end
