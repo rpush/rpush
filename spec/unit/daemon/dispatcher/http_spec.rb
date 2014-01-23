89e5bd197e7eaad99d883ade66bba0af31ec5313
Ian Leitch <port001@gmail.com>
2014-01-22 18:16:58 -0800

    Add specs for Http and Tcp dispatchers.

--- spec/unit/daemon/dispatcher/http_spec.rb
+++ spec/unit/daemon/dispatcher/http_spec.rb
@@ -1,5 +1,33 @@
 require 'unit_spec_helper'
 
 describe Rapns::Daemon::Dispatcher::Http do
-  it 'needs specs'
+  let(:app) { double }
+  let(:delivery_class) { double }
+  let(:notification) { double }
+  let(:batch) { double }
+  let(:http) { double }
+  let(:dispatcher) { Rapns::Daemon::Dispatcher::Http.new(app, delivery_class) }
+
+  before { Net::HTTP::Persistent.stub(:new => http) }
+
+  it 'constructs a new persistent connection' do
+    Net::HTTP::Persistent.should_receive(:new)
+    Rapns::Daemon::Dispatcher::Http.new(app, delivery_class)
+  end
+
+  describe 'dispatch' do
+    it 'delivers the notification' do
+      delivery = double
+      delivery_class.should_receive(:new).with(app, http, notification, batch).and_return(delivery)
+      delivery.should_receive(:perform)
+      dispatcher.dispatch(notification, batch)
+    end
+  end
+
+  describe 'cleanup' do
+    it 'closes the connection' do
+      http.should_receive(:shutdown)
+      dispatcher.cleanup
+    end
+  end
 end
--- spec/unit/daemon/dispatcher/tcp_spec.rb
+++ spec/unit/daemon/dispatcher/tcp_spec.rb
@@ -1,5 +1,38 @@
 require 'unit_spec_helper'
 
 describe Rapns::Daemon::Dispatcher::Tcp do
-  it 'needs specs'
+  let(:app) { double }
+  let(:delivery) { double(:perform => nil) }
+  let(:delivery_class) { double(:new => delivery) }
+  let(:notification) { double }
+  let(:batch) { double }
+  let(:connection) { double(Rapns::Daemon::TcpConnection, :connect => nil) }
+  let(:host) { 'localhost' }
+  let(:port) { 1234 }
+  let(:host_proc) { Proc.new { |app| [host, port] } }
+  let(:dispatcher) { Rapns::Daemon::Dispatcher::Tcp.new(app, delivery_class, :host => host_proc) }
+
+  before { Rapns::Daemon::TcpConnection.stub(:new => connection) }
+
+  describe 'dispatch' do
+    it 'lazily connects the socket' do
+      Rapns::Daemon::TcpConnection.should_receive(:new).with(app, host, port).and_return(connection)
+      connection.should_receive(:connect)
+      dispatcher.dispatch(notification, batch)
+    end
+
+    it 'delivers the notification' do
+      delivery_class.should_receive(:new).with(app, connection, notification, batch).and_return(delivery)
+      delivery.should_receive(:perform)
+      dispatcher.dispatch(notification, batch)
+    end
+  end
+
+  describe 'cleanup' do
+    it 'closes the connection' do
+      dispatcher.dispatch(notification, batch) # lazily initialize connection
+      connection.should_receive(:close)
+      dispatcher.cleanup
+    end
+  end
 end
