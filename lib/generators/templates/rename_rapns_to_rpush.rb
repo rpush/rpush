1279a8566f3e742c8c62ba96450e149269154262
Ian Leitch <port001@gmail.com>
2014-02-02 14:05:09 -0800

    Rename app type also.

--- lib/generators/templates/rename_rapns_to_rpush.rb
+++ lib/generators/templates/rename_rapns_to_rpush.rb
@@ -1,8 +1,16 @@
 class RenameRapnsToRpush < ActiveRecord::Migration
   module Rpush
+    class App < ActiveRecord::Base
+      self.table_name = 'rpush_apps'
+    end
+
     class Notification < ActiveRecord::Base
       self.table_name = 'rpush_notifications'
     end
+  end
+
+  def self.update_type(model, from, to)
+    model.where(type: from).update_all(type: to)
   end
 
   def self.up
@@ -13,17 +21,27 @@ class RenameRapnsToRpush < ActiveRecord::Migration
     rename_index :rpush_notifications, :index_rapns_notifications_multi, :index_rpush_notifications_multi
     rename_index :rpush_feedback, :index_rapns_feedback_on_device_token, :index_rpush_feedback_on_device_token
 
-    RenameRapnsToRpush::Rpush::Notification.where(type: 'Rapns::Apns::Notification').update_all(type: 'Rpush::Apns::Notification')
-    RenameRapnsToRpush::Rpush::Notification.where(type: 'Rapns::Gcm::Notification').update_all(type: 'Rpush::Gcm::Notification')
-    RenameRapnsToRpush::Rpush::Notification.where(type: 'Rapns::Adm::Notification').update_all(type: 'Rpush::Adm::Notification')
-    RenameRapnsToRpush::Rpush::Notification.where(type: 'Rapns::Wpns::Notification').update_all(type: 'Rpush::Wpns::Notification')
+    update_type(RenameRapnsToRpush::Rpush::Notification, 'Rapns::Apns::Notification', 'Rpush::Apns::Notification')
+    update_type(RenameRapnsToRpush::Rpush::Notification, 'Rapns::Gcm::Notification', 'Rpush::Gcm::Notification')
+    update_type(RenameRapnsToRpush::Rpush::Notification, 'Rapns::Adm::Notification', 'Rpush::Adm::Notification')
+    update_type(RenameRapnsToRpush::Rpush::Notification, 'Rapns::Wpns::Notification', 'Rpush::Wpns::Notification')
+
+    update_type(RenameRapnsToRpush::Rpush::App, 'Rapns::Apns::App', 'Rpush::Apns::App')
+    update_type(RenameRapnsToRpush::Rpush::App, 'Rapns::Gcm::App', 'Rpush::Gcm::App')
+    update_type(RenameRapnsToRpush::Rpush::App, 'Rapns::Adm::App', 'Rpush::Adm::App')
+    update_type(RenameRapnsToRpush::Rpush::App, 'Rapns::Wpns::App', 'Rpush::Wpns::App')
   end
 
   def self.down
-    RenameRapnsToRpush::Rpush::Notification.where(type: 'Rpush::Apns::Notification').update_all(type: 'Rapns::Apns::Notification')
-    RenameRapnsToRpush::Rpush::Notification.where(type: 'Rpush::Gcm::Notification').update_all(type: 'Rapns::Gcm::Notification')
-    RenameRapnsToRpush::Rpush::Notification.where(type: 'Rpush::Adm::Notification').update_all(type: 'Rapns::Adm::Notification')
-    RenameRapnsToRpush::Rpush::Notification.where(type: 'Rpush::Wpns::Notification').update_all(type: 'Rapns::Wpns::Notification')
+    update_type(RenameRapnsToRpush::Rpush::Notification, 'Rpush::Apns::Notification', 'Rapns::Apns::Notification')
+    update_type(RenameRapnsToRpush::Rpush::Notification, 'Rpush::Gcm::Notification', 'Rapns::Gcm::Notification')
+    update_type(RenameRapnsToRpush::Rpush::Notification, 'Rpush::Adm::Notification', 'Rapns::Adm::Notification')
+    update_type(RenameRapnsToRpush::Rpush::Notification, 'Rpush::Wpns::Notification', 'Rapns::Wpns::Notification')
+
+    update_type(RenameRapnsToRpush::Rpush::App, 'Rpush::Apns::App', 'Rapns::Apns::App')
+    update_type(RenameRapnsToRpush::Rpush::App, 'Rpush::Gcm::App', 'Rapns::Gcm::App')
+    update_type(RenameRapnsToRpush::Rpush::App, 'Rpush::Adm::App', 'Rapns::Adm::App')
+    update_type(RenameRapnsToRpush::Rpush::App, 'Rpush::Wpns::App', 'Rapns::Wpns::App')
 
     rename_index :rpush_notifications, :index_rpush_notifications_multi, :index_rapns_notifications_multi
     rename_index :rpush_feedback, :index_rpush_feedback_on_device_token, :index_rapns_feedback_on_device_token
