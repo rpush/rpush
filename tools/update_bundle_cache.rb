bbc7cd3cb4306095c5f02be62cdd9902e53a18ee
Ian Leitch <port001@gmail.com>
2014-02-19 10:47:10 -0800

    Cache all gems

---
+++ tools/update_bundle_cache.rb
@@ -0,0 +1,14 @@
+require 'yaml'
+require 'fileutils'
+
+FileUtils.rm_rf("vendor/cache")
+
+travis_yml = YAML.load(File.read(".travis.yml"))
+
+travis_yml["rvm"].each do |ruby|
+    travis_yml["gemfile"].each do |gemfile|
+        cmd = "export BUNDLE_GEMFILE=#{gemfile}; rvm #{ruby} do bundle package --all --no-prune"
+        puts cmd
+        puts `#{cmd}`
+    end
+end
