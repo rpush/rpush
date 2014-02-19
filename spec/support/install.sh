4535fe769fccea18a642214fd095753cc386c5aa
Ian Leitch <port001@gmail.com>
2014-02-19 15:42:02 -0800

    Run nc in the background [ci-skip]

--- spec/support/install.sh
+++ spec/support/install.sh
@@ -50,5 +50,5 @@ while true do
   n.reload
 end
 EOF
-nc -l 80 < /rpush/spec/integration/support/gcm_success_response.json
+nc -l 80 < /rpush/spec/integration/support/gcm_success_response.json &
 rails runner check_notification_status.rb
