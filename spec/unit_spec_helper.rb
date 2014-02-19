8724e99f2d6e14acb7ffd0733fd84ad59ef634c5
Ian Leitch <port001@gmail.com>
2014-02-19 11:31:02 -0800

    Fix transactions for Rails 4

--- spec/unit_spec_helper.rb
+++ spec/unit_spec_helper.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
+require 'rails'
 
 def is_unit_example?(example)
   path = example.metadata[:example_group][:file_path]
   path =~ /spec\/unit/
+end
+
+def rails4?
+  ::Rails::VERSION::STRING >= '4'
 end
 
 RSpec.configure do |config|
@@ -16,18 +21,28 @@ RSpec.configure do |config|
   config.before(:each) do
     if is_unit_example?(example)
       connection = ActiveRecord::Base.connection
-      connection.increment_open_transactions
-      connection.transaction_joinable = false
-      connection.begin_db_transaction
+
+      if rails4?
+        connection.begin_transaction joinable: false
+      else
+        connection.increment_open_transactions
+        connection.transaction_joinable = false
+        connection.begin_db_transaction
+      end
     end
   end
 
   config.after(:each) do
     if is_unit_example?(example)
       connection = ActiveRecord::Base.connection
-      if connection.open_transactions != 0
-        connection.rollback_db_transaction
-        connection.decrement_open_transactions
+
+      if rails4?
+        connection.rollback_transaction if connection.transaction_open?
+      else
+        if connection.open_transactions != 0
+          connection.rollback_db_transaction
+          connection.decrement_open_transactions
+        end
       end
     end
   end
