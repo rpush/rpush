321367693048d59aee228d8551dcafc41e441422
Ian Leitch <port001@gmail.com>
2014-04-19 18:45:07 -0700

    Update cache.

--- vendor/cache/modis-dc9b26473dbe/spec/support/simplecov_quality_formatter.rb
+++
@@ -1,8 +0,0 @@
-class SimpleCov::Formatter::QualityFormatter
-  def format(result)
-    SimpleCov::Formatter::HTMLFormatter.new.format(result)
-    File.open("coverage/covered_percent", "w") do |f|
-      f.puts result.source_files.covered_percent.to_f
-    end
-  end
-end
