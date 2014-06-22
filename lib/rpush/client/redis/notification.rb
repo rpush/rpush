46354d5af8a7243e9e6f26f6a1e382de57f9fbc4
Ian Leitch <port001@gmail.com>
2014-06-21 18:18:13 -0700

    Default sound to 'default'.

--- lib/rpush/client/redis/notification.rb
+++ lib/rpush/client/redis/notification.rb
@@ -20,7 +20,7 @@ module Rpush
 
         attribute :badge, :integer
         attribute :device_token, :string
-        attribute :sound, :string
+        attribute :sound, :string, default: 'default'
         attribute :alert, :hash, strict: false
         attribute :data, :hash
         attribute :expiry, :integer, default: 1.day.to_i
