e8fac7d862f09285379607bbd032612368643904
Ian Leitch <port001@gmail.com>
2014-06-21 18:50:09 -0700

    Revert "Default alert in Redis to a string"
    This reverts commit e1dae71224796e27b3e24664f75a410c15f82ebb.

--- lib/rpush/client/redis/apns/notification.rb
+++ lib/rpush/client/redis/apns/notification.rb
@@ -4,11 +4,6 @@ module Rpush
       module Apns
         class Notification < Rpush::Client::Redis::Notification
           include Rpush::Client::ActiveModel::Apns::Notification
-
-          def alert=(alert)
-            alert = alert.to_s unless alert.kind_of?(Hash)
-            super
-          end
         end
       end
     end
