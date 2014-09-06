3fd0d52e65be51cc81c95c0d1790bd9c788775fd
Ian Leitch <port001@gmail.com>
2014-09-06 15:59:50 -0700

    Re-work signal handling to use IO.select.

--- lib/rpush/daemon/signal_handler.rb
+++ lib/rpush/daemon/signal_handler.rb
@@ -7,40 +7,40 @@ module Rpush
 
       def self.start
         return unless trap_signals?
-        @shutting_down = false
+
         read_io, @write_io = IO.pipe
         start_handler(read_io)
         %w(INT TERM HUP USR2).each do |signal|
-          Signal.trap(signal) { @write_io.write("#{Signal.list[signal]}\n") }
+          Signal.trap(signal) { @write_io.puts(signal) }
         end
       end
 
       def self.stop
-        @write_io.write("shutdown\n") if @write_io
+        @write_io.puts('break') if @write_io
         @thread.join if @thread
       end
 
       def self.start_handler(read_io)
         @thread = Thread.new do
-          loop do
-            case read_io.readline.strip.to_i
-            when Signal.list['HUP']
+          while readable_io = IO.select([read_io]) # rubocop:disable AssignmentInCondition
+            signal = readable_io.first[0].gets.strip
+
+            case signal
+            when 'HUP'
               Synchronizer.sync
               Feeder.wakeup
-            when Signal.list['USR2']
+            when 'USR2'
               AppRunner.debug
-            when Signal.list['INT'], Signal.list['TERM']
-              Thread.new { handle_shutdown_signal }
-            else
+            when 'INT', 'TERM'
+              Thread.new { Rpush::Daemon.shutdown }
               break
+            when 'break'
+              break
+            else
+              Rpush.logger.error("Unhandled signal: #{signal}")
             end
           end
         end
-      end
-
-      def self.handle_shutdown_signal
-        @shutting_down = true
-        Rpush::Daemon.shutdown
       end
 
       def self.trap_signals?
