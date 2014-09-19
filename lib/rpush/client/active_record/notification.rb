447245d8c1173c6abb8a3d519347407e4e15d507
Ian Leitch <port001@gmail.com>
2014-09-18 19:01:34 -0700

    Add category to attr_accessible.

--- lib/rpush/client/active_record/notification.rb
+++ lib/rpush/client/active_record/notification.rb
@@ -16,7 +16,7 @@ module Rpush
           attr_accessible :badge, :device_token, :sound, :alert, :data, :expiry, :delivered,
                           :delivered_at, :failed, :failed_at, :error_code, :error_description, :deliver_after,
                           :alert_is_json, :app, :app_id, :collapse_key, :delay_while_idle, :registration_ids,
-                          :uri, :url_args
+                          :uri, :url_args, :category
         end
 
         def data=(attrs)
