fc0c7b5bf20854d51dc018666853448a74109e1a
Ian Leitch <port001@gmail.com>
2015-02-19 12:59:21 -0800

    Handle exceptions when joining sleep thread in InterruptibleSleep.

--- lib/rpush/daemon/interruptible_sleep.rb
+++ lib/rpush/daemon/interruptible_sleep.rb
@@ -1,17 +1,22 @@
-require 'monitor'
-
 module Rpush
   module Daemon
     class InterruptibleSleep
       def sleep(duration)
         @thread = Thread.new { Kernel.sleep duration }
         Thread.pass
-        @thread.join
+
+        begin
+          @thread.join
+        rescue StandardError # rubocop:disable Lint/HandleExceptions
+          @thread = nil
+        end
       end
 
       def stop
         @thread.kill if @thread
       rescue StandardError # rubocop:disable Lint/HandleExceptions
+      ensure
+        @thread = nil
       end
     end
   end
