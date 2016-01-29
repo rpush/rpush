1b954873a612f6b4d63ae8639f67b497d5bf006f
Anton Rieder <aried3r@gmail.com>
2016-01-29 19:00:52 +0100

    Add priority to GCM

--- lib/rpush/client/active_model/gcm/notification.rb
+++ lib/rpush/client/active_model/gcm/notification.rb
@@ -3,9 +3,14 @@ module Rpush
     module ActiveModel
       module Gcm
         module Notification
+          GCM_PRIORITY_HIGH = Rpush::Client::ActiveModel::Apns::Notification::APNS_PRIORITY_IMMEDIATE
+          GCM_PRIORITY_NORMAL = Rpush::Client::ActiveModel::Apns::Notification::APNS_PRIORITY_CONSERVE_POWER
+          GCM_PRIORITIES = [GCM_PRIORITY_HIGH, GCM_PRIORITY_NORMAL]
+
           def self.included(base)
             base.instance_eval do
               validates :registration_ids, presence: true
+              validates :priority, inclusion: { in: GCM_PRIORITIES }, allow_nil: true
 
               validates_with Rpush::Client::ActiveModel::PayloadDataSizeValidator, limit: 4096
               validates_with Rpush::Client::ActiveModel::RegistrationIdsCountValidator, limit: 1000
@@ -14,16 +19,37 @@ module Rpush
             end
           end
 
+          # This is a hack. The schema defines `priority` to be an integer, but GCM expects a string.
+          # But for users of rpush to have an API they might expect (setting priority to `high`, not 10)
+          # we do a little conversion here.
+          # I'm not happy about it, but this will have to do until I can take a further look.
+          def priority=(priority)
+            case priority
+              when 'high'
+                super(GCM_PRIORITY_HIGH)
+              when 'normal'
+                super(GCM_PRIORITY_NORMAL)
+              else
+                errors.add(:priority, 'must be one of either "normal" or "high"')
+            end
+          end
+
           def as_json(options = nil)
             json = {
-              'registration_ids' => registration_ids,
-              'delay_while_idle' => delay_while_idle,
-              'data' => data
+                'registration_ids' => registration_ids,
+                'delay_while_idle' => delay_while_idle,
+                'data' => data
             }
             json['collapse_key'] = collapse_key if collapse_key
             json['time_to_live'] = expiry if expiry
             json['content_available'] = content_available if content_available
+            json['priority'] = priority_for_notification if priority
             json
+          end
+
+          def priority_for_notification
+            return 'high' if priority == GCM_PRIORITY_HIGH
+            'normal' if priority == GCM_PRIORITY_NORMAL
           end
         end
       end
