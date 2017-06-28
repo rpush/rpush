b2d9722a45e27577e36d485d26155b3905e0e2fd
Anton Rieder <aried3r@gmail.com>
2017-06-28 14:27:01 +0200

    Revert "Bundle rpush-redis in main repo"
    This reverts commit 1c94d9242af0635b5091554f7080a8c6f3b74c96.

--- rpush-redis/lib/rpush/redis.rb
+++
@@ -1,7 +0,0 @@
-require "rpush/redis/version"
-
-module Rpush
-  module Redis
-    # Your code goes here...
-  end
-end
--- rpush-redis/lib/rpush/redis/version.rb
+++
@@ -1,5 +0,0 @@
-module Rpush
-  module Redis
-    VERSION = '0.4.1'
-  end
-end
