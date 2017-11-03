37427245186de0e35981c898f62f861221e4f62c
Marian Negru <marian@icabbi.com>
2017-11-03 12:04:33 +0200

    Added missing require for specs

--- spec/support/active_record_setup.rb
+++ spec/support/active_record_setup.rb
@@ -32,6 +32,7 @@ require 'generators/templates/rpush_2_1_0_updates'
 require 'generators/templates/rpush_2_6_0_updates'
 require 'generators/templates/rpush_2_7_0_updates'
 require 'generators/templates/rpush_3_0_0_updates'
+require 'generators/templates/add_apnsp8'
 
 migrations = [AddRpush, Rpush200Updates, Rpush210Updates, Rpush260Updates, Rpush270Updates, Rpush300Updates, AddApnsp8]
 
