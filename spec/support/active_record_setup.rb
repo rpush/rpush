6ff186e19a414e5990fe8f2faeb98b5c7e04cadf
Anton Rieder <aried3r@gmail.com>
2018-01-08 14:24:34 +0100

    Add 3.0.1 migration to test suite

--- spec/support/active_record_setup.rb
+++ spec/support/active_record_setup.rb
@@ -32,8 +32,17 @@ require 'generators/templates/rpush_2_1_0_updates'
 require 'generators/templates/rpush_2_6_0_updates'
 require 'generators/templates/rpush_2_7_0_updates'
 require 'generators/templates/rpush_3_0_0_updates'
+require 'generators/templates/rpush_3_0_1_updates'
 
-migrations = [AddRpush, Rpush200Updates, Rpush210Updates, Rpush260Updates, Rpush270Updates, Rpush300Updates]
+migrations = [
+  AddRpush,
+  Rpush200Updates,
+  Rpush210Updates,
+  Rpush260Updates,
+  Rpush270Updates,
+  Rpush300Updates,
+  Rpush301Updates
+]
 
 unless ENV['TRAVIS']
   migrations.reverse_each do |m|
