4d0c6438a5406ceeef739a97331a8537f81bc055
Gerard Morera <grdmorera@gmail.com>
2018-10-25 17:27:07 +0200

    Add thread_id column to rpush_notifications

--- lib/generators/rpush_migration_generator.rb
+++ lib/generators/rpush_migration_generator.rb
@@ -48,6 +48,7 @@ class RpushMigrationGenerator < Rails::Generators::Base
     add_rpush_migration('rpush_3_1_1_updates')
     add_rpush_migration('rpush_3_2_0_add_apns_p8')
     add_rpush_migration('rpush_3_2_4_updates')
+    add_rpush_migration('rpush_3_3_0_updates')
   end
 
   protected
