00d1b234869b9624393842fdd978e664e6c1e01b
Jess Hottenstein <jess@splitwise.com>
2019-09-27 15:24:45 -0400

    Extract common tests into a shared example

--- spec/unit_spec_helper.rb
+++ spec/unit_spec_helper.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'rails'
 
 # load all shared example files
-Dir["./spec/unit/client/**/shared/**/*.rb"].sort.each { |f| require f }
+Dir["./spec/unit/**/shared/**/*.rb"].sort.each { |f| require f }
 
 def unit_example?(metadata)
   metadata[:file_path] =~ %r{spec/unit}
