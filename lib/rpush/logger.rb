47fd4bbd6d1e5bc125690c5ba017b42ead34f50c
Greg Blake <greg.blake@powerhrg.com>
2021-12-27 17:08:15 -0500

    Resolves Rails 7 Time.now.to_s deprecation warning

--- lib/rpush/logger.rb
+++ lib/rpush/logger.rb
@@ -69,7 +69,7 @@ module Rpush
         msg = "#{msg.class.name}, #{msg.message}\n#{formatted_backtrace}"
       end
 
-      formatted_msg = "[#{Time.now.to_s(:db)}] "
+      formatted_msg = "[#{Time.now.to_formatted_s(:db)}]"
       formatted_msg << '[rpush] ' if Rpush.config.embedded
       formatted_msg << "[#{prefix}] " if prefix
       formatted_msg << msg
