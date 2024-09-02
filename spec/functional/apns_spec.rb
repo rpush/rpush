d51626ba4b313f07343e091636cb5d184fee9647
Ben Langfeld <blangfeld@powerhrg.com>
2024-09-02 19:23:34 -0300

    Drop functional test unhappy path

--- spec/functional/apns_spec.rb
+++ spec/functional/apns_spec.rb
@@ -5,9 +5,6 @@ describe 'APNs' do
   let(:tcp_socket) { double(TCPSocket, setsockopt: nil, close: nil) }
   let(:ssl_socket) { double(OpenSSL::SSL::SSLSocket, :sync= => nil, connect: nil, write: nil, flush: nil, read: nil, close: nil) }
   let(:io_double) { double(select: nil) }
-  let(:delivered_ids) { [] }
-  let(:failed_ids) { [] }
-  let(:retry_ids) { [] }
 
   before do
     Rpush.config.push_poll = 0.5
@@ -32,78 +29,11 @@ describe 'APNs' do
     notification
   end
 
-  def wait
-    sleep 0.1
-  end
-
-  def wait_for_notification_to_deliver(notification)
-    timeout { wait until delivered_ids.include?(notification.id) }
-  end
-
-  def wait_for_notification_to_fail(notification)
-    timeout { wait until failed_ids.include?(notification.id) }
-  end
-
-  def wait_for_notification_to_retry(notification)
-    timeout { wait until retry_ids.include?(notification.id) }
-  end
-
-  def fail_notification(notification)
-    allow(ssl_socket).to receive_messages(read: [8, 4, notification.id].pack('ccN'))
-    enable_io_select
-  end
-
-  def enable_io_select
-    called = false
-    allow(io_double).to receive(:select) do
-      if called
-        nil
-      else
-        called = true
-      end
-    end
-  end
-
   it 'delivers a notification successfully' do
     notification = create_notification
     expect do
       Rpush.push
       notification.reload
     end.to change(notification, :delivered).to(true)
-  end
-
-  describe 'delivery failures' do
-    before do
-      Rpush.reflect do |on|
-        on.notification_delivered do |n|
-          delivered_ids << n.id
-        end
-
-        on.notification_id_failed do |_, n_id|
-          failed_ids << n_id
-        end
-
-        on.notification_id_will_retry do |_, n_id|
-          retry_ids << n_id
-        end
-
-        on.notification_will_retry do |n|
-          retry_ids << n.id
-        end
-      end
-
-      Rpush.embed
-    end
-
-    after do
-      timeout { Rpush.shutdown }
-    end
-
-    it 'fails to deliver a notification' do
-      notification = create_notification
-      wait_for_notification_to_deliver(notification)
-      fail_notification(notification)
-      wait_for_notification_to_fail(notification)
-    end
   end
 end
