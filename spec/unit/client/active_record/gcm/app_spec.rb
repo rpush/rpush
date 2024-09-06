4eda5c7a57fe116f0a10561fca6b90f61b2ccf84
Ben Langfeld <blangfeld@powerhrg.com>
2024-09-06 17:36:14 -0300

    Remove GCM implementation (#688)
    This was shut down by Google in August 2024 and replaced by FCM
    (supported in RPush 8.0.0).

--- spec/unit/client/active_record/gcm/app_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'unit_spec_helper'
-
-describe Rpush::Client::ActiveRecord::Gcm::App do
-  it_behaves_like 'Rpush::Client::Gcm::App'
-  it_behaves_like 'Rpush::Client::ActiveRecord::App'
-end if active_record?
