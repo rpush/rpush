4c85a5c5160f275003a72bc0b2e6cebf0a743497
Ben Langfeld <blangfeld@powerhrg.com>
2024-09-19 15:58:08 -0300

    Backward compatibility

--- lib/rpush/client/active_record/notification.rb
+++ lib/rpush/client/active_record/notification.rb
@@ -7,7 +7,7 @@ module Rpush
 
         self.table_name = 'rpush_notifications'
 
-        self.default_column_serializer = YAML
+        self.default_column_serializer = YAML if respond_to?(:default_column_serializer)
 
         serialize :registration_ids
         serialize :url_args
